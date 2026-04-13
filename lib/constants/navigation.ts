ad435d3c776a01214c08a93c13f4ae930335f799
furkanarslan1 <furkanarslandev@gmail.com>
2026-04-13 14:26:36 +0300

    feat:add header

---
+++ lib/constants/navigation.ts
@@ -0,0 +1,18 @@
+export const navigations = [
+  {
+    name: "Anasayfa",
+    href: "/",
+  },
+  {
+    name: "Hizmetlerimiz",
+    href: "/hizmetlerimiz",
+  },
+  {
+    name: "Hakkımızda",
+    href: "/hakkimizda",
+  },
+  {
+    name: "İletişim",
+    href: "/iletisim",
+  },
+];
